b8ab460d2683ea2b806883351168b76850d45ad6
Allan Guerra <allang@ciandt.com>
2020-03-03 08:28:13 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -33,4 +33,20 @@ describe('SignUpController', () => {
     expect(response).toHaveProperty('body')
     expect(response.body).toEqual(new MissingParamError('email'))
   })
+
+  test('should return 400 if no name is provided', () => {
+    // sut == 'System Under Test' - name pattern to difer class under test from others
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'any_email@mail.com'
+      }
+    }
+    const response = sut.handle(httpRequest)
+    expect(response).toHaveProperty('statusCode')
+    expect(response.statusCode).toBe(400)
+    expect(response).toHaveProperty('body')
+    expect(response.body).toEqual(new MissingParamError('password'))
+  })
 })
