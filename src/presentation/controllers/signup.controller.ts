8d84dcaa15965fa0d217e5e6b223b952b2871e5e
Allan Guerra <allang@ciandt.com>
2020-03-03 08:35:52 -0300

    refactor: create Controller protocol

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,8 +1,9 @@
 import { HttpResponse, HttpRequest } from '../protocols/http.protocol'
 import { MissingParamError } from '../errors/missing-param.error'
 import { BadRequest } from '../helpers/http.helper'
+import { Controller } from '../protocols/controller.protocol'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (req: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
---
+++ src/presentation/protocols/controller.protocol.ts
@@ -0,0 +1,5 @@
+import { HttpResponse, HttpRequest } from '../protocols/http.protocol'
+
+export interface Controller {
+  handle (req: HttpRequest): HttpResponse
+}
