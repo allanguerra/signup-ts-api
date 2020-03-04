89dd0896afece05bd265907d841db76bbac6b912
Allan Guerra <allang@ciandt.com>
2020-03-04 15:40:59 -0300

    test: ensure DbStoreAccount calls StoreAccountRepository with correct data

--- src/data/use-cases/add-account/db-store-account.spec.ts
+++ src/data/use-cases/add-account/db-store-account.spec.ts
@@ -1,9 +1,26 @@
 import { DbStoreAccount } from './db-store-account'
-import { Encrypter } from './db-store-account-protocols'
+import { Encrypter, StoreAccountRepository, StoreAccountModel } from './db-store-account-protocols'
+import { AccountModel } from '../../../domain/models/account.model'
 
 interface SutTypes {
   sut: DbStoreAccount
   encrypterStub: Encrypter
+  storeAccountRepositoryStub: StoreAccountRepository
+}
+
+const makeStoreAccountRepository = (): StoreAccountRepository => {
+  class StoreAccountRepositoryStub implements StoreAccountRepository {
+    async store (account: StoreAccountModel): Promise<AccountModel> {
+      const fakeAccount = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'hashed_password'
+      }
+      return new Promise(resolve => resolve(fakeAccount))
+    }
+  }
+  return new StoreAccountRepositoryStub()
 }
 
 const makeEncrypter = (): Encrypter => {
@@ -16,11 +33,13 @@ const makeEncrypter = (): Encrypter => {
 }
 
 const makeSUT = (): SutTypes => {
+  const storeAccountRepositoryStub = makeStoreAccountRepository()
   const encrypterStub = makeEncrypter()
-  const sut = new DbStoreAccount(encrypterStub)
+  const sut = new DbStoreAccount(encrypterStub, storeAccountRepositoryStub)
   return {
     sut,
-    encrypterStub
+    encrypterStub,
+    storeAccountRepositoryStub
   }
 }
 
@@ -47,5 +66,21 @@ describe('DbStoreAccount UseCase', () => {
     jest.spyOn(encrypterStub, 'encrypt').mockRejectedValue(new Error())
     const promise = sut.store(accountData)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should call StoreAccountRepository with correct data', async () => {
+    const { sut, storeAccountRepositoryStub } = makeSUT()
+    const storeSpy = jest.spyOn(storeAccountRepositoryStub, 'store')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'p@ssW0rd'
+    }
+    await sut.store(accountData)
+    expect(storeSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
   })
 })
