de97e7779dcb7bf22bf066c4b8e1354939c39dce
Allan Guerra <allang@ciandt.com>
2020-03-04 13:14:28 -0300

    refactor: move sut creation to a method

--- src/utils/email-validator.adapter.spec.ts
+++ src/utils/email-validator.adapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSUT = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email Validator Adapter', () => {
   test('should return false if EmailValidator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSUT()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
   test('should return true if EmailValidator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSUT()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   test('should call EmailValidator whit correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSUT()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
