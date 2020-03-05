e33ecf8f0402bfa77828d53c43529724d929e425
Allan Guerra <allang@ciandt.com>
2020-03-05 14:03:52 -0300

    refactor: move id mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,8 +7,6 @@ export class AccountMongoRepository implements StoreAccountRepository {
   async store (accountData: StoreAccountModel): Promise<AccountModel> {
     const accountsCollection = MongoHelper.getCollection('accounts')
     const result = await accountsCollection.insertOne(accountData)
-    const account = result.ops[0]
-    const { _id, ...accountWithoutId } = account
-    return Object.assign({}, accountWithoutId, { id: _id })
+    return MongoHelper.map(result.ops[0])
   }
 }
