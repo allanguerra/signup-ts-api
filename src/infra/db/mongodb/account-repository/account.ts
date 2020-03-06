ceb551e3a3a5e4c115755a55b9289af396c9f13d
Allan Guerra <allang@ciandt.com>
2020-03-06 11:07:45 -0300

    feat: ensure MongoDB reconnects if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongo.helper'
 
 export class AccountMongoRepository implements StoreAccountRepository {
   async store (accountData: StoreAccountModel): Promise<AccountModel> {
-    const accountsCollection = MongoHelper.getCollection('accounts')
+    const accountsCollection = await MongoHelper.getCollection('accounts')
     const result = await accountsCollection.insertOne(accountData)
     return MongoHelper.map(result.ops[0])
   }
