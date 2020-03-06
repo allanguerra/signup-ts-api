5fdff4a9165138ca3bdeb304d2772e8bd7a72567
Allan Guerra <allang@ciandt.com>
2020-03-06 13:56:04 -0300

    test: ensure LogControllerDecorator calls Controller with correct body

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,50 @@
+import { LogControllerDecorator } from './log'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+describe('Log Controller Decorator', () => {
+  interface sutTypes {
+    sut: LogControllerDecorator
+    controllerStub: Controller
+  }
+
+  const makeControllerStub = (): Controller => {
+    class ControllerStub implements Controller {
+      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse = {
+          statusCode: 200,
+          body: {
+            id: 'any_id',
+            name: 'any_name',
+            email: 'any_email@gmail.com',
+            password: 'any_password'
+          }
+        }
+        return new Promise(resolve => resolve(httpResponse))
+      }
+    }
+    return new ControllerStub()
+  }
+
+  const makeSUT = (): sutTypes => {
+    const controllerStub = makeControllerStub()
+    const sut = new LogControllerDecorator(controllerStub)
+    return {
+      sut,
+      controllerStub
+    }
+  }
+
+  test('should call controller handle', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    const { sut, controllerStub } = makeSUT()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
