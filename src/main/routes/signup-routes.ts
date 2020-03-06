c3f0fe29164c94a292e621c89acb0d8b3b5d012c
Allan Guerra <allang@ciandt.com>
2020-03-06 10:42:21 -0300

    refactor: add signup controller handler

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
+import { makeSignUpController } from '../factories/signup'
+import { adaptRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
   router
-    .post('/signup', (req, res) => {
-      res.json({ ok: 'ok' })
-    })
+    .post('/signup', adaptRoute(makeSignUpController()))
 }
